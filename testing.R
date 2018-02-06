4575ae6bea837d806c8c8881ddb80c044f51142f
Sarah Schwartz <sarah.schwartz@usu.edu>
2018-02-06 16:00:03 -0700

    toc fix

--- testing.R
+++ testing.R
@@ -1,6 +1,9 @@
-bookdown::render_book(c("index.Rmd","00-install.Rmd", 
-                        "01-prep.Rmd", "02-plots.Rmd", 
-                        "03-summary.Rmd", "04-normal.Rmd",
+bookdown::render_book(c("index.Rmd",
+                        "00-install.Rmd", 
+                        "01-prep.Rmd", 
+                        "02-plots.Rmd", 
+                        "03-summary.Rmd", 
+                        "04-normal.Rmd",
                         "05-assumptions.Rmd",
                         "06-t_test.Rmd",
                         "99-refs.Rmd"))
